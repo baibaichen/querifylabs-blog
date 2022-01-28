1953d1ad3b041527562ea8f9c544ac336d29771b
Chang Chen <baibaichen@gmail.com>
2022-01-28 18:30:22 +0800

    type system

--- 03-spark-calcite/src/main/java/io/apache/kylin/calcite/catalog/SparkSchemaTable.java
+++ 03-spark-calcite/src/main/java/io/apache/kylin/calcite/catalog/SparkSchemaTable.java
@@ -1,12 +1,15 @@
 package io.apache.kylin.calcite.catalog;
 
+import io.apache.kylin.calcite.impl.SparkTypeFactory;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.impl.AbstractTable;
 
 import org.apache.spark.sql.connector.catalog.Table;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class SparkSchemaTable extends AbstractTable {
 
@@ -18,6 +21,8 @@ public class SparkSchemaTable extends AbstractTable {
 
     @Override
     public RelDataType getRowType(RelDataTypeFactory typeFactory) {
+        final SparkTypeFactory sparkTypeFactory = (SparkTypeFactory) typeFactory;
+        Arrays.stream(sparkTable.schema().fields()).forEach(sparkTypeFactory::checkX);
         return null;
     }
 
--- 03-spark-calcite/src/main/java/io/apache/kylin/calcite/impl/SparkTypeFactory.java
+++ 03-spark-calcite/src/main/java/io/apache/kylin/calcite/impl/SparkTypeFactory.java
@@ -1,12 +1,15 @@
 package io.apache.kylin.calcite.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
+import org.apache.spark.sql.types.StructField;
 
 /**
  * Spark specific type factory that represents the interface between Spark's {@link org.apache.spark.sql.types.DataType DataType}
  * and Calcite's {@link org.apache.calcite.rel.type.RelDataType RelDataType}.
  */
+@Slf4j
 public class SparkTypeFactory extends JavaTypeFactoryImpl {
 
     public SparkTypeFactory() {
@@ -15,5 +18,9 @@ public class SparkTypeFactory extends JavaTypeFactoryImpl {
 
     public SparkTypeFactory(RelDataTypeSystem typeSystem) {
         super(typeSystem);
+    }
+
+    public void checkX(StructField field) {
+      log.info(field.toString());
     }
 }
